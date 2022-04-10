8b5d62fa651757bb5065ad1daa89288c0df78815
Mathenge-Alex <arxmaina@gmail.com>
2022-04-10 12:55:27 +0300

    add and initialize quote class

---
+++ src/app/quotes.ts
@@ -0,0 +1,9 @@
+export class Quotes {
+    constructor(public quote: number,public author: string, public upvote:number, public downvote:number, public datePast: Date){
+        this.quote = quote
+        this.author = author
+        this.upvote = upvote
+        this.downvote = downvote
+        this.datePast = datePast
+    }
+}
