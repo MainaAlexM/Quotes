726bab3a5dba6f9abf559a20287d890dbbe5ed58
Mathenge-Alex <arxmaina@gmail.com>
2022-04-11 08:50:16 +0300

    add topQuote highlight directive

---
+++ src/app/top-quote.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TopQuoteDirective } from './top-quote.directive';
+
+describe('TopQuoteDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TopQuoteDirective();
+    expect(directive).toBeTruthy();
+  });
+});
