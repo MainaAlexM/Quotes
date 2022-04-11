97fbb040b383207a2ccb8c2a3cba41eb3f4efa5f
Mathenge-Alex <arxmaina@gmail.com>
2022-04-11 09:02:20 +0300

    add quote delete function

--- src/app/quote-block/quote-info/quote-info.component.ts
+++ src/app/quote-block/quote-info/quote-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 import { Quotes } from 'src/app/quotes';
 
 @Component({
@@ -9,6 +9,15 @@ import { Quotes } from 'src/app/quotes';
 export class QuoteInfoComponent implements OnInit {
   @Input()
   quote!: Quotes;
+  @Output() toDelete = new EventEmitter<boolean>();
+
+  
+  deleteItem(done:boolean){
+    this.toDelete.emit(done);
+  }
+
+
+
   constructor() { }
 
   ngOnInit(): void {
