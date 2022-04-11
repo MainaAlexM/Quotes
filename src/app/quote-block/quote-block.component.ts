ac88f2769104711bf745b14a3d86b0db3e60b6a1
Mathenge-Alex <arxmaina@gmail.com>
2022-04-11 08:42:18 +0300

    add quotes listing function

--- src/app/quote-block/quote-block.component.ts
+++ src/app/quote-block/quote-block.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quote-block',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-block.component.css']
 })
 export class QuoteBlockComponent implements OnInit {
-
+  quotes:Quotes[] = [
+    new Quotes ("If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.", "James Cameron", 0, 0, new Date(27,7,1992)),
+    new Quotes ("Life is what happens when you're busy making other plans.", "John Lennon", 1, 0, new Date(17,12,2004)),
+    new Quotes ("Whoever is happy will make others happy too.", "Anne Frank", 0, 0, new Date(10,18,2017)),
+    new Quotes ("The secret of getting ahead is getting started", "Mark Twain", 0, 0, new Date(4,8,2014))
+  ]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
-    constructor(public quote: string,public author: string, public upvote:number, public downvote:number, public datePast: Date){
-        this.quote = quote
+    constructor(public statement: string,public author: string, public upvote:number, public downvote:number, public datePast: Date){
+        this.statement = statement
         this.author = author
         this.upvote = upvote
         this.downvote = downvote
