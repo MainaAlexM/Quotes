58d5dd2be1c320d9ee01eb48bf28d7ebb780261a
Mathenge-Alex <arxmaina@gmail.com>
2022-04-12 07:13:46 +0300

    Feat: add time past after posting logic

--- src/app/dating.pipe.ts
+++ src/app/dating.pipe.ts
@@ -5,8 +5,33 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatingPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, args?: any): any {
+    if (value) {
+        const pastSeconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+
+        if (pastSeconds < 59)
+            return 'Just now';
+        const durations: { [key: string]: number } = {
+            'year': 31536000,
+            'month': 2592000,
+            'week': 604800,
+            'day': 86400,
+            'hour': 3600,
+            'minute': 60,
+            'second': 1
+        };
+        let timeSpan;
+        for (const i in durations) {
+            timeSpan = Math.floor(pastSeconds / durations[i]);
+            if (timeSpan > 0)
+                if (timeSpan === 1) {
+                    return `$(timeSpan) $(i) ago`;
+                } else {
+                    return timeSpan + ' ' + i + 's ago';
+                }
+        }
+    }
+    return value;
   }
 
 }
