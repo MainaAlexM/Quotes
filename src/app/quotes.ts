007dc0db8112eddda455b1f487ef81b3df6e30fb
Mathenge-Alex <arxmaina@gmail.com>
2022-04-12 07:37:59 +0300

    add id on quote constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,5 +1,6 @@
 export class Quotes {
-    constructor(public statement: string,public author: string, public upvote:number, public downvote:number, public datePast: Date){
+    constructor(public id: number, public statement: string,public author: string, public upvote:number, public downvote:number, public datePast: Date){
+        this.id = id
         this.statement = statement
         this.author = author
         this.upvote = upvote
